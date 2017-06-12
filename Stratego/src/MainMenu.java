f5b5e86037f44f465a0dd1f83ea6d32ff2649cc0
DESKTOP-8C8U9B9\user <programatisths@gmail.com>
2017-06-12 14:21:09 +0300

    Added sound on board Buttons

--- Stratego/src/MainMenu.java
+++ Stratego/src/MainMenu.java
@@ -1943,6 +1943,17 @@ public class MainMenu extends JFrame{
 		
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
+			
+			try {
+				InputStream in = new FileInputStream("music/bars.wav");
+	        	AudioStream as = new AudioStream(in);         
+	        	AudioPlayer.player.start(as); 
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (IOException p) {
+				p.printStackTrace();
+			}
+			
 			Position[][] pos = Game.getInstance().getBoard();
 			
 			int p = list.getSelectedIndex();
@@ -2509,6 +2520,17 @@ public class MainMenu extends JFrame{
 	class ButtonListener1 implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
+			
+			try {
+				InputStream in = new FileInputStream("music/bars.wav");
+	        	AudioStream as = new AudioStream(in);         
+	        	AudioPlayer.player.start(as); 
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (IOException p) {
+				p.printStackTrace();
+			}
+			
 			int x = 0;
 			String playerRED = "RED";
 			String playerBLUE = "BLUE";
